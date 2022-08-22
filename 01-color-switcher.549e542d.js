52cdc7d67d733918d6c076dbce31000131e5a105
IhorBorkaliuk <99047545+IhorBorkaliuk@users.noreply.github.com>
2022-08-22 18:57:59 +0000

    Deploying to gh-pages from @ IhorBorkaliuk/goit-js-hw-09@7e176c6a91133a1ca00eef3f5f93911cc53212fd 🚀

---
+++ 01-color-switcher.549e542d.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=(document.querySelector("body"),null);t.disabled=!0,e.addEventListener("click",(function(){e.disabled=!0,t.disabled=!1,d=setInterval((function(){document.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3)})),t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,clearInterval(d)}))}();
+//# sourceMappingURL=01-color-switcher.549e542d.js.map
