ae92cd221552e3744aa5263211eaf2bfb3e01c3d
Ihor Borkaliuk <happyviolence1990@gmail.com>
2022-08-22 17:18:15 +0200

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,80 @@
+const btnStart = document.querySelector('button[data-start]')
+const btnStop = document.querySelector('button[data-stop]')
+const body = document.querySelector('body')
 
+let timeOutID = null
+
+btnStop.disabled = true
+
+btnStart.addEventListener('click', () => {
+    btnStart.disabled = true
+    btnStop.disabled = false
+
+    timeOutID = setInterval( () => {
+       document.body.style.backgroundColor = getRandomHexColor() 
+    }, 1000)
+})
+
+btnStop.addEventListener('click', () => {
+    btnStart.disabled = false
+    btnStop.disabled = true
+    clearInterval(timeOutID)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
